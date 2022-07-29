5aaafc97539930477ec8c52d9618538d43adaa9c
Bogdan Rudko <rydkoc@icloud.com>
2022-07-29 16:51:03 +0300

    fix hiber util

--- src/main/java/com/ersted/util/HibernateUtil.java
+++ src/main/java/com/ersted/util/HibernateUtil.java
@@ -2,7 +2,12 @@ package com.ersted.util;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class HibernateUtil {
     private static SessionFactory sessionFactory;
@@ -11,8 +16,19 @@ public class HibernateUtil {
     }
 
     public static SessionFactory getSessionFactory(){
-        if(sessionFactory == null || sessionFactory.isClosed())
-            sessionFactory = new Configuration().configure().buildSessionFactory();
+        if(sessionFactory == null || sessionFactory.isClosed()){
+            Map<String,String> jdbcUrlSettings = new HashMap<>();
+            String jdbcDbUrl = System.getenv("JDBC_DATABASE_URL");
+            if (null != jdbcDbUrl) {
+                jdbcUrlSettings.put("hibernate.connection.url", System.getenv("JDBC_DATABASE_URL"));
+            }
+
+            StandardServiceRegistry build = new StandardServiceRegistryBuilder().
+                    configure("hibernate.cfg.xml").
+                    applySettings(jdbcUrlSettings).
+                    build();
+            sessionFactory = new Configuration().configure().buildSessionFactory(build);
+        }
         return sessionFactory;
     }
 
